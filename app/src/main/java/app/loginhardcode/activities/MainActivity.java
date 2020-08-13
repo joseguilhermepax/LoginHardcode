e7d07b13cce1035b7c39d82f6b84a476a5d18a01
José Guilherme <jose.guilherme.pax@gmail.com>
2020-08-12 23:42:41 -0300

    V1: Tela inicial

---
+++ app/src/main/java/app/loginhardcode/activities/MainActivity.java
@@ -0,0 +1,16 @@
+package app.loginhardcode.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import app.loginhardcode.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
